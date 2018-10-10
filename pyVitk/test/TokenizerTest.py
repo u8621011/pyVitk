36ef6a5efd3011fab36db69c5eb763197b7b56b9
Ted Chen <shapable.ted@gmail.com>
2018-10-10 10:02:42 +0800

    add tokenize line test case for several sentences

--- pyVitk/test/TokenizerTest.py
+++ pyVitk/test/TokenizerTest.py
@@ -3,7 +3,7 @@
 from unittest import TestCase
 import logging
 from pyVitk.helper import setup_logging
-from pyVitk.Tokenizer import Tokenizer, PhraseGraph, PhraseGraph2, SegmentationFunction
+from pyVitk.Tokenizer import Tokenizer, PhraseGraph, SegmentationFunction
 
 # setup the logger
 setup_logging()
@@ -23,9 +23,12 @@ class TokenzierTestCase(TestCase):
         cls.userTokenizer = None
         logger.debug('End TokenizerTest')
 
+    def assert_expected_tokens(self, tokens, expections):
+        for t, e in zip(tokens, expections):
+            self.assertEqual(t.text, e)
+
     def test_phrase_graph_1(self):
         graph = PhraseGraph(self.tokenizer)
-        graph2 = PhraseGraph2(self.tokenizer)
 
         s = 'Wikipedia tiếng Việt là phiên bản tiếng Việt của dự án Wikipedia'
 
@@ -38,17 +41,8 @@ class TokenzierTestCase(TestCase):
         p = graph.shortestPaths()
         logger.debug('Graph Shortest paths Result: {}'.format(p))
 
-        # PhraseGraph2
-        graph2.makeGraph(s)
-        logger.debug('Graph2 Making Result: {}'.format(graph2))
-
-        p = graph2.shortestPath()
-        logger.debug('Graph2 Shortest paths Result: {}'.format(p))
-        "hay là một tổ_chức quốc_tế hoạt_động phi lợi_nhuận phi_chính_phủ và bao_gồm các thành_viên có trình_độ"
-
     def test_phrase_graph_2(self):
         graph = PhraseGraph(self.tokenizer)
-        graph2 = PhraseGraph2(self.tokenizer)
 
         s = 'là phiên bản tiếng'
 
@@ -61,16 +55,9 @@ class TokenzierTestCase(TestCase):
         p = graph.shortestPaths()
         logger.debug('Graph Shortest Paths: {}'.format(p))
 
-        #graph2
-        graph2.makeGraph(s)
-        logger.debug('Graph2 making result: {}'.format(graph2))
-
-        p = graph2.shortestPath()
-        logger.debug('Graph2 Shortest Paths: {}'.format(p))
 
     def test_phrase_graph_3(self):
         graph = PhraseGraph(self.tokenizer)
-        graph2 = PhraseGraph2(self.tokenizer)
 
         s = 'gần đây nhất mình thi'
 
@@ -82,13 +69,6 @@ class TokenzierTestCase(TestCase):
         p = graph.shortestPaths()
         logger.debug('Shortest Paths: {}'.format(p))
 
-        # graph 2
-        graph2.makeGraph(s)
-        logger.debug('Graph2 making result: {}'.format(graph2))
-
-        p = graph2.shortestPath()
-        logger.debug('Shortest Paths: {}'.format(p))
-
     def test_tokenize_phrase(self):
         s = 'là phiên bản tiếng Việt'
         seg = SegmentationFunction(self.tokenizer)
@@ -97,94 +77,72 @@ class TokenzierTestCase(TestCase):
         logger.debug('Tokenized Phrase: {}'.format(lst))
 
     def test_tokenize_line(self):
-        s = 'Wikipedia tiếng Việt là phiên bản tiếng Việt của dự án Wikipedia.'
-        t = self.tokenizer.tokenizeLine(s)
+        lines = [
+            'là ngôn ngữ có nguồn gốc bản địa',
+            'Wikipedia tiếng Việt là phiên bản tiếng Việt của dự án Wikipedia.',
+            'Website được kích hoạt lần đầu tiên vào tháng 11 năm 2002, lúc đó chỉ có bài viết đầu tiên của dự án là bài Internet Society.[1] ',
+            'Vì dự án không có đủ người đóng góp, Wikipedia tiếng Việt không có thêm bài viết nào cho đến tháng 10 năm 2003 khi Trang Chính được viết,[2] và dự án Wikipedia tiếng Việt được xem như "khởi động lại nữa".',
+            'bắt đi bộ thế này',
+            'nhỏ*',
+            'gần đây nhất mình thi',
+            '• Con gái của ba hả.↵• Dạ con chào ba.',
+            'Tiết độ sứ Tiết tiếu lâm tiềm Lực.',
+            'PHD | Xem Đi Xem Lại Cả 1000 Lần Mà Vẫn Không Thể Nhịn Được Cười | Funny Videos,',
+            'BÍCH PHƯƠNG - Bao Giờ Lấy Chồng? [OFFICIAL M/V]',
+            'Kem Xôi TV: Tập 74 - Dài bao nhiêu là đủ,',
+        ]
+        expected = [
+            ['là',  'ngôn ngữ',  'có',  'nguồn gốc', 'bản địa'],
+            ['Wikipedia', 'tiếng', 'Việt', 'là', 'phiên bản', 'tiếng', 'Việt', 'của', 'dự án', 'Wikipedia', '.'],
+            ['Website', 'được', 'kích hoạt', 'lần', 'đầu tiên', 'vào', 'tháng', '11', 'năm', '2002', ',', 'lúc', 'đó', 'chỉ', 'có', 'bài', 'viết', 'đầu tiên', 'của', 'dự án', 'là', 'bài', 'Internet', 'Society', '.', '[', '1', ']'],
+            ['Vì', 'dự án', 'không', 'có', 'đủ', 'người', 'đóng góp', ',', 'Wikipedia', 'tiếng', 'Việt', 'không', 'có', 'thêm', 'bài', 'viết', 'nào', 'cho', 'đến', 'tháng', '10', 'năm', '2003', 'khi', 'Trang', 'Chính', 'được', 'viết', ',', '[', '2', ']', 'và', 'dự án', 'Wikipedia', 'tiếng', 'Việt', 'được', 'xem', 'như', '"', 'khởi động', 'lại', 'nữa', '".'],
+            ['bắt', 'đi', 'bộ', 'thế', 'này'],
+            ['nhỏ', '*'],
+            ['gần', 'đây', 'nhất', 'mình', 'thi'],
+            ['•', 'Con', 'gái', 'của', 'ba', 'hả', '.', '↵', '•', 'Dạ', 'con', 'chào', 'ba', '.'],
+            ['Tiết', 'độ', 'sứ', 'Tiết', 'tiếu lâm', 'tiềm', 'Lực', '.'],
+            ['PHD', '|', 'Xem', 'Đi', 'Xem', 'Lại', 'Cả', '1000', 'Lần', 'Mà', 'Vẫn', 'Không', 'Thể', 'Nhịn', 'Được', 'Cười', '|', 'Funny', 'Videos', ','],
+            ['BÍCH', 'PHƯƠNG', '-', 'Bao', 'Giờ', 'Lấy', 'Chồng', '?', '[', 'OFFICIAL', 'M/V', ']', ],
+            ['Kem', 'Xôi', 'TV', ':', 'Tập', '74', '-', 'Dài', 'bao nhiêu', 'là', 'đủ', ',']
+        ]
 
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
+        for s, e in zip(lines, expected):
+            t = self.tokenizer.tokenizeLine(s)
+            self.assert_expected_tokens(t, e)
 
-    def test_tokenize_line2(self):
-        s = 'Website được kích hoạt lần đầu tiên vào tháng 11 năm 2002, lúc đó chỉ có bài viết đầu tiên của dự án là bài Internet Society.[1] '
-        t = self.tokenizer.tokenizeLine(s)
+    def test_tokenize_line_concat(self):
+        lines = [
+            'là ngôn ngữ có nguồn gốc bản địa',
+            'Wikipedia tiếng Việt là phiên bản tiếng Việt của dự án Wikipedia.',
+            'Website được kích hoạt lần đầu tiên vào tháng 11 năm 2002, lúc đó chỉ có bài viết đầu tiên của dự án là bài Internet Society.[1] ',
+            'Vì dự án không có đủ người đóng góp, Wikipedia tiếng Việt không có thêm bài viết nào cho đến tháng 10 năm 2003 khi Trang Chính được viết,[2] và dự án Wikipedia tiếng Việt được xem như "khởi động lại nữa".',
+            'bắt đi bộ thế này',
+            'nhỏ*',
+            'gần đây nhất mình thi',
+            '• Con gái của ba hả.↵• Dạ con chào ba.',
+            'Tiết độ sứ Tiết tiếu lâm tiềm Lực.',
+            'PHD | Xem Đi Xem Lại Cả 1000 Lần Mà Vẫn Không Thể Nhịn Được Cười | Funny Videos,',
+            'BÍCH PHƯƠNG - Bao Giờ Lấy Chồng? [OFFICIAL M/V]',
+            'Kem Xôi TV: Tập 74 - Dài bao nhiêu là đủ,',
+        ]
+        expected = [
+            ['là',  'ngôn_ngữ',  'có',  'nguồn_gốc', 'bản_địa'],
+            ['Wikipedia', 'tiếng', 'Việt', 'là', 'phiên_bản', 'tiếng', 'Việt', 'của', 'dự_án', 'Wikipedia', '.'],
+            ['Website', 'được', 'kích_hoạt', 'lần', 'đầu_tiên', 'vào', 'tháng', '11', 'năm', '2002', ',', 'lúc', 'đó', 'chỉ', 'có', 'bài', 'viết', 'đầu_tiên', 'của', 'dự_án', 'là', 'bài', 'Internet', 'Society', '.', '[', '1', ']'],
+            ['Vì', 'dự_án', 'không', 'có', 'đủ', 'người', 'đóng_góp', ',', 'Wikipedia', 'tiếng', 'Việt', 'không', 'có', 'thêm', 'bài', 'viết', 'nào', 'cho', 'đến', 'tháng', '10', 'năm', '2003', 'khi', 'Trang', 'Chính', 'được', 'viết', ',', '[', '2', ']', 'và', 'dự_án', 'Wikipedia', 'tiếng', 'Việt', 'được', 'xem', 'như', '"', 'khởi_động', 'lại', 'nữa', '".'],
+            ['bắt', 'đi', 'bộ', 'thế', 'này'],
+            ['nhỏ', '*'],
+            ['gần', 'đây', 'nhất', 'mình', 'thi'],
+            ['•', 'Con', 'gái', 'của', 'ba', 'hả', '.', '↵', '•', 'Dạ', 'con', 'chào', 'ba', '.'],
+            ['Tiết', 'độ', 'sứ', 'Tiết', 'tiếu_lâm', 'tiềm', 'Lực', '.'],
+            ['PHD', '|', 'Xem', 'Đi', 'Xem', 'Lại', 'Cả', '1000', 'Lần', 'Mà', 'Vẫn', 'Không', 'Thể', 'Nhịn', 'Được', 'Cười', '|', 'Funny', 'Videos', ','],
+            ['BÍCH', 'PHƯƠNG', '-', 'Bao', 'Giờ', 'Lấy', 'Chồng', '?', '[', 'OFFICIAL', 'M/V', ']', ],
+            ['Kem', 'Xôi', 'TV', ':', 'Tập', '74', '-', 'Dài', 'bao_nhiêu', 'là', 'đủ', ',']
+        ]
 
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenzie_line3(self):
-        s = 'Vì dự án không có đủ người đóng góp, Wikipedia tiếng Việt không có thêm bài viết nào cho đến tháng 10 năm 2003 khi Trang Chính được viết,[2] và dự án Wikipedia tiếng Việt được xem như "khởi động lại nữa".'
-        t = self.tokenizer.tokenizeLine(s)
-
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenzie_line4(self):
-        s = 'là ngôn ngữ có nguồn gốc bản địa'
-
-        t = self.tokenizer.tokenizeLine(s)
-
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line5(self):
-        s = 'bắt đi bộ thế này'
-
-        t = self.tokenizer.tokenizeLine(s)
-
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line6(self):
-        s = 'nhỏ*'
-
-        t = self.tokenizer.tokenizeLine(s)
-
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line7(self):
-        s = "gần đây nhất mình thi"
-
-        t = self.tokenizer.tokenizeLine(s)
-
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line8(self):
-        s = '• Con gái của ba hả.↵• Dạ con chào ba.'
-        t = self.tokenizer.tokenizeLine(s)
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line9(self):
-        s = 'Tiết độ sứ Tiết tiếu lâm tiềm Lực.'
-        t = self.tokenizer.tokenizeLine(s)
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-        t = self.tokenizer_insensitive.tokenizeLine(s)
-        logger.debug('Test insensitive tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line10(self):
-        s = "PHD | Xem Đi Xem Lại Cả 1000 Lần Mà Vẫn Không Thể Nhịn Được Cười | Funny Videos,"
-        t = self.tokenizer.tokenizeLine(s)
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line11(self):
-        s = "BÍCH PHƯƠNG - Bao Giờ Lấy Chồng? [OFFICIAL M/V]"
-        t = self.tokenizer.tokenizeLine(s)
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
-
-    def test_tokenize_line12(self):
-        s = "Kem Xôi TV: Tập 74 - Dài bao nhiêu là đủ,"
-        self.print_tokenization(s)
-
-    def print_tokenization(self, s):
-        t = self.tokenizer.tokenizeLine(s)
-        logger.debug('Test tokenizeline, s: ' + s)
-        logger.debug('Tokenized Result: {}'.format(t))
+        for s, e in zip(lines, expected):
+            t = self.tokenizer.tokenizeLine(s, concat=True)
+            self.assert_expected_tokens(t, e)
 
     def test_serialize_to_xml(self):
         self.tokenizer.to_lexicon_xml_file('test.xml')

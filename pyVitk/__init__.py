6bce49597326cf8e8f587b71e6225787616fee96
Ted Chen <shapable.ted@gmail.com>
2018-10-10 10:02:09 +0800

    remove the logger config in module init codes

--- pyVitk/__init__.py
+++ pyVitk/__init__.py
@@ -3,16 +3,6 @@
 from .Tokenizer import Tokenizer
 import pyVitk.crawler
 from .DictionaryLexicon import DictionaryLexicon
-import logging
-
-logger = logging.getLogger(__name__)
-if not len(logger.handlers):
-    # file handler
-    hdlr = logging.FileHandler('pyVitk.log', encoding='utf8')
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    hdlr.setFormatter(formatter)
-    logger.addHandler(hdlr) 
-    logger.setLevel(logging.DEBUG)
 
 name = "pyVitk"
 
